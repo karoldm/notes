90ad02d6d78a4cf8a63a2d5df7954ae3aabd277f
karol marques <karoldm1579@gmail.com>
2024-02-11 17:36:02 -0300

    refactor: unused vars and imports

--- src/app.tsx
+++ src/app.tsx
@@ -88,6 +88,7 @@ function App() {
         <form className='w-full'>
           <input 
             onChange={handleSearch}
+            value={search}
             type='text' 
             placeholder='Busque em suas notas...' 
             className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
